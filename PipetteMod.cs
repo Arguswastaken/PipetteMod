e4400ea2db203b768c205f960bae3bcee535e69b
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-06-18 13:31:14 -0500

    Add some global uisngs

--- PipetteMod.cs
+++ PipetteMod.cs
@@ -1,4 +1,8 @@
-using Terraria.ModLoader;
+global using System;
+global using System.Linq;
+global using Terraria;
+global using Terraria.UI;
+global using Terraria.ModLoader;
 
 namespace PipetteMod;
 
